fd393cbddebb20e08afeb696de085db37397010f
Rolf <rolf.wessels@gmail.com>
2018-06-17 14:47:52 +0200

    Use graphQL for the dashboard.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -8,6 +8,10 @@ import 'rxjs/add/observable/empty' ;
 import { ProjectService } from '../../@core/data/projects/projects.service';
 import { Project } from '../../@core/data/projects/project.model';
 import { LastUpdateModel } from '../../@theme/components/last-updated/last-updated.component';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Query } from '@angular/compiler/src/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-dashboard',
@@ -15,7 +19,9 @@ import { LastUpdateModel } from '../../@theme/components/last-updated/last-updat
 })
 export class DashboardComponent {
   users: Observable<LastUpdateModel[]>;
+  usersCount: Observable<number>;
   projects: Observable<LastUpdateModel[]>;
+  projectsCount: Observable<number>;
   hasValidToken: boolean;
   user: Observable<User>;
 
@@ -23,17 +29,52 @@ export class DashboardComponent {
     private authService: NbAuthService,
     private userService: UserService,
     private projectService: ProjectService,
+    apollo: Apollo,
   ) {
+
+    const recentData = apollo.query({
+        query: gql`
+        {
+          projects {
+            recent(first:5){
+              id,
+              name,
+              updateDate
+            }
+            query {
+              count
+            }
+          }
+          users {
+            recent(first:5) {
+              id,
+              name,
+              email,
+              updateDate
+            }
+            query {
+              count
+            }
+          }
+        }`,
+      });
+
+
     this.authService.onTokenChange()
+
       .subscribe((token: OAuth2Token) => {
         this.hasValidToken = token.isValid();
         if (this.hasValidToken) {
           this.user = this.userService.getWhoami();
-          this.users = this.userService.getAllDetail()
+          this.users = recentData
+          .pipe(map(x => <User[]> (<any>x.data).users.recent))
           .map((users: User[]) => users.map(u => new LastUpdateModel(u.id, u.name, u.updateDate, u.email)));
+          this.usersCount = recentData.pipe(map(x => <number> (<any>x.data).users.query.count));
 
-          this.projects = this.projectService.getAllDetail()
-            .map((users: Project[]) => users.map(u => new LastUpdateModel(u.id, u.name, u.updateDate)));
+          this.projects = recentData
+          .pipe(map(x => <Project[]> (<any>x.data).projects.recent))
+          .map((project: Project[]) => project.map(u => new LastUpdateModel(u.id, u.name, u.updateDate)));
+          this.projectsCount = recentData.pipe(map(x => <number> (<any>x.data).projects.query.count));
         }
       });
   }
